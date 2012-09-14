7420240edfc41c0b7768f605caf734a85cc31be2
Liz Sheffield <izzebot@live.unc.edu>
2012-09-14 15:20:33 -0400

    Liz's Test
    :-D

---
+++ Liz's Test/Liz's Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Liz's Test
+//
+//  Created by ResNET on 9/14/12.
+//  Copyright (c) 2012 ResNET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Liz's Test/Liz's TestTests/Liz_s_TestTests.m
@@ -0,0 +1,32 @@
+//
+//  Liz_s_TestTests.m
+//  Liz's TestTests
+//
+//  Created by ResNET on 9/14/12.
+//  Copyright (c) 2012 ResNET. All rights reserved.
+//
+
+#import "Liz_s_TestTests.h"
+
+@implementation Liz_s_TestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Liz's TestTests");
+}
+
+@end
